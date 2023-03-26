9c59eb09b1f58151611feb3027c9261dc46b03fb
Boazthefirst <117732045+boazctrlc@users.noreply.github.com>
2023-03-26 16:08:51 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,388 @@
+/* Quiz source: w3schools.com */
+var quiz = {
+    "JS": [
+    {
+    "id": 1,
+    "question": "מה היא הטומאה הכי גדולה?",
+    "options": [
+    {
+    "a": "טומאת מת",
+    "b": "טומאת נבלה",
+    "c": "טומאת צרעת",
+    }
+    ],
+    "answer": "טומאת מת",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 2,
+    "question": "איזה ארץ הכי קדושה?",
+    "options": [
+    {
+    "a": "ארצות הברית",
+    "b": "גרמניה",
+    "c": "טורקיה",
+    "d": "ישראל"
+    }
+    ],
+    "answer": "ישראל",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 3,
+    "question": "מה המשמעות של מועד לפי הרב הופמן?",
+    "options": [
+    {
+    "a": "כל זמן שהוא",
+    "b": "יום מקודש שחוגגים פעם בשנה",
+    "c": "א, ב נכונות"
+    }
+    ],
+    "answer": "א, ב נכונות",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 4,
+    "question": "למה מיועד הביטוי חג בתורה?",
+    "options": [
+    {
+    "a": "לכל מועד מיוחד",
+    "b": "לשלושת הרגלים",
+    }
+    ],
+    "answer": "לשלושת הרגלים",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 5,
+    "question": "באיזה עוד שם התורה קוראת למועדים?",
+    "options": [
+    {
+    "a": "חג",
+    "b": "יום טוב",
+    "c": "מקרא קודש",
+    "d": "זמן",
+    }
+    ],
+    "answer": "מקרא קודש",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 6,
+    "question": "מה המועד הראשון המוזכר בויקרא פרק כג?",
+    "options": [
+    {
+    "a": "פסח",
+    "b": "שבת",
+    "c": "סוכות",
+    }
+    ],
+    "answer": "שבת",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 7,
+    "question": "איזה חג חל 50 יום אחרי פסח?",
+    "options": [
+    {
+    "a": "יום העצמאות",
+    "b": "לג בעומר",
+    "c": "שבועות",
+    }
+    ],
+    "answer": "שבועות",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 8,
+    "question": "מה עושים עם חולה צרעת במדבר?",
+    "options": [
+    {
+    "a": "מוציאים מהמחנה",
+    "b": "סוקלים באבנים ",
+    "c": "טבילה במקווה",
+    "d": "מוכר את כל רכושו",
+    }
+    ],
+    "answer": "מוציאים מהמחנה",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 9,
+    "question": "איזה יום הוא לא יום מיוחד מהתורה?",
+    "options": [
+    {
+    "a": "שבת",
+    "b": "יום כיפור",
+    "c": "פורים",
+    }
+    ],
+    "answer": "פורים",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 10,
+    "question": "כמה זמן חולה צרעת צריך לחכות מחוץ למחנה?",
+    "options": [
+    {
+    "a": "שבועיים",
+    "b": "יומיים",
+    "c": "שבוע",
+    "d": " ארבעה ימים",
+    }
+    ],
+    "answer": "שבוע",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 11,
+    "question": "מה היתה תגובת אהרון למת בניו?",
+    "options": [
+    {
+    "a": "כעס וזעם על ה",
+    "b": "בא בתלונות למשה שהוא עשה את זה" ,
+    "c": "עמד בדממה",
+    "d": "הגן על שאר בניו שלא יפגעו"
+    }
+    ],
+    "answer": "עמד בדממה",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 12,
+    "question": "לפי הפשט מה היתה הבעיה בהקרבה של נדב ואביהוא?",
+    "options": [
+    {
+    "a": "הם הקריבו כאשר היו שיכורים",
+    "b": "הם הקריבו אש זרה אשר לא נצטוו עליה",
+    "c": "הם הקריב קרבן עולה במקום חטאת",
+    "d": "הם סירבו להקשיב למשה ואהרון"
+    }
+    ],
+    "answer": "הם הקריבו אש זרה אשר לא נצטוו עליה",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 13,
+    "question": "נדב ואביהוא שאפו לקדושה ביום מיוחד, מה קרה ביום הזה",
+    "options": [
+    {
+    "a": "שנים עשר הנשיאים הקריבו את קורבנם",
+    "b": "ה' סלח לישראל אחרי חטא העגל",
+    "c": "קרבן ראשון עלה על המזבח",
+    "d": "משה ואהרון חנכו את המשכן וה' הוריד אש לאכול את הקורבנות"
+    }
+    ],
+    "answer": "משה ואהרון חנכו את המשכן וה' הוריד אש לאכול את הקורבנות",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 14,
+    "question": "מה זה אקספוזיציה?",
+    "options": [
+    {
+    "a": "סוף הסיפור",
+    "b": "תחילת הסיפור",
+    "c": "תיאור הדמויות"
+    }
+    ],
+    "answer": "תחילת הסיפור",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 15,
+    "question": "מה זה אנלוגיה ניגודית?",
+    "options": [
+    {
+    "a": "הדגשה של שוני בין שני דברים אף על פי הדמיון ביניהם",
+    "b": "אמצעי ספרותי שמדגיש את מה שלא כתוב",
+    }
+    ],
+    "answer": "הדגשה של שוני בין שני דברים אף על פי הדמיון ביניהם",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 16,
+    "question": "מה זה אנלוגיה ניגודית?",
+    "options": [
+    {
+    "a": "הדגשה של שוני בין שני דברים אף על פי הדמיון ביניהם",
+    "b": "אמצעי ספרותי שמדגיש את מה שלא כתוב",
+    }
+    ],
+    "answer": "הדגשה של שוני בין שני דברים אף על פי הדמיון ביניהם",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 17,
+    "question": "האם מותר לעקור עץ פרי??",
+    "options": [
+    {
+    "a": "כן",
+    "b": "לא",
+    }
+    ],
+    "answer": "לא",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 18,
+    "question": "איזה ברכות לא כלולות בשכר של התורה?",
+    "options": [
+    {
+    "a": "גשמים",
+    "b": "שלום",
+    "c": "חוכמה",
+    "d": "ביטחון"
+    }
+    ],
+    "answer": "חוכמה;",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 19,
+    "question": "למה תהילה לא התחתנה בפעם בראשונה?",
+    "options": [
+    {
+    "a": "גילו שהוא עני",
+    "b": "הוא היה חסיד",
+    "c": "אמא שלו בדיוק נפטרה",
+    "d": "הוא נהיה חולה"
+    }
+    ],
+    "answer": "הוא היה חסיד",
+    "score": 0,
+    "status": ""
+    },
+    {
+    "id": 20,
+    "question": "מי הוא הסופר של תהילה?",
+    "options": [
+    {
+    "a": "אריק איינשטיין",
+    "b": "אהרון אמיר",
+    "c": "שי עגנון"
+    }
+    ],
+    "answer": "שי עגנון",
+    "score": 0,
+    "status": ""
+    },
+    ]
+    }
+    var quizApp = function () {
+    this.score = 0;
+    this.qno = 1;
+    this.currentque = 0;
+    var totalque = quiz.JS.length;
+    this.displayQuiz = function (cque) {
+    this.currentque = cque;
+    if (this.currentque < totalque) {
+    $("#tque").html(totalque);
+    $("#previous").attr("disabled", false);
+    $("#next").attr("disabled", false);
+    $("#qid").html(quiz.JS[this.currentque].id + '.');
+    $("#question").html(quiz.JS[this.currentque].question);
+    $("#question-options").html("");
+    for (var key in quiz.JS[this.currentque].options[0]) {
+    if (quiz.JS[this.currentque].options[0].hasOwnProperty(key)) {
+    $("#question-options").append(
+    "<div class='form-check option-block'>" +
+    "<label class='form-check-label'>" +
+    "<input type='radio' class='form-check-input' name='option' id='q" + key + "' value='" + quiz.JS[this.currentque].options[0][key] + "'><span id='optionval'>" +
+    quiz.JS[this.currentque].options[0][key] +
+    "</span></label>"
+    );
+    }
+    }
+    }
+    if (this.currentque <= 0) {
+    $("#previous").attr("disabled", true);
+    }
+    if (this.currentque >= totalque) {
+    $('#next').attr('disabled', true);
+    for (var i = 0; i < totalque; i++) {
+    this.score = this.score + quiz.JS[i].score;
+    }
+    return this.showResult(this.score);
+    }
+    }
+    this.showResult = function (scr) {
+    $("#result").addClass('result');
+    $("#result").html("<h1 class='res-header'>ציון סופי: &nbsp;" + scr + '/' + totalque + "</h1>");
+    for (var j = 0; j < totalque; j++) {
+    var res;
+    if (quiz.JS[j].score == 0) {
+    res = '<span class="wrong">' + quiz.JS[j].score + '</span><i class="fa fa-remove c-wrong"></i>';
+    } else {
+    res = '<span class="correct">' + quiz.JS[j].score + '</span><i class="fa fa-check c-correct"></i>';
+    }
+    $("#result").append(
+    '<div class="result-question"><span>Q ' + quiz.JS[j].id + '</span> &nbsp;' + quiz.JS[j].question + '</div>' +
+    '<div><b>תשובה נכונה:</b> &nbsp;' + quiz.JS[j].answer + '</div>' +
+    '<div class="last-row"><b>ציון:</b> &nbsp;' + res +
+    '</div>'
+    );
+    }
+    }
+    this.checkAnswer = function (option) {
+    var answer = quiz.JS[this.currentque].answer;
+    option = option.replace(/</g, "&lt;") //for <
+    option = option.replace(/>/g, "&gt;") //for >
+    option = option.replace(/"/g, "&quot;")
+    if (option == quiz.JS[this.currentque].answer) {
+    if (quiz.JS[this.currentque].score == "") {
+    quiz.JS[this.currentque].score = 1;
+    quiz.JS[this.currentque].status = "correct";
+    }
+    } else {
+    quiz.JS[this.currentque].status = "wrong";
+    }
+    }
+    this.changeQuestion = function (cque) {
+    this.currentque = this.currentque + cque;
+    this.displayQuiz(this.currentque);
+    }
+    }
+    var jsq = new quizApp();
+    var selectedopt;
+    $(document).ready(function () {
+    jsq.displayQuiz(0);
+    $('#question-options').on('change', 'input[type=radio][name=option]', function (e) {
+    //var radio = $(this).find('input:radio');
+    $(this).prop("checked", true);
+    selectedopt = $(this).val();
+    });
+    });
+    $('#next').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(1);
+    });
+    $('#previous').click(function (e) {
+    e.preventDefault();
+    if (selectedopt) {
+    jsq.checkAnswer(selectedopt);
+    }
+    jsq.changeQuestion(-1);
+    });
